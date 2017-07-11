b222fd7182dfdf1515e7dc8c2f765f7799e12212
cxwang001 <william@Williams-MBP.attlocal.net>
2017-07-10 21:56:40 -0700

    twitter

---
+++ liri.js
@@ -0,0 +1,5 @@
+var fs = require("fs");							
+var keys = require("./keys.js");				
+var twitter = require("twitter");					
+var request = require("request");				
+var spotify = require("spotify");				
