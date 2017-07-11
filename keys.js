5cf4c51a46587d77dc42876a09de4aaaf13e0827
cxwang001 <william@Williams-MBP.attlocal.net>
2017-07-10 21:49:30 -0700

    twitter

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'U4mc8vqhC8Mi6P948Bl8fMDfn',
+  consumer_secret: 'ty2j4LtSHmLA33n84htmPcIkPzY3XiRKJvmv2SSrr4ZbojKjEn',
+  access_token_key: '3158108360-fOZh08TnbL3TW27DXAGVUzVGE8Xn5qqZoCZ3u00',
+  access_token_secret: 'm1yjyFUbV36bB8mtjcAdIA3cNsvA2Q8qNpJm728WJzLPP',
+}
